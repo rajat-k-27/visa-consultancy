ce500e8f543b14ae03925f9339c62c9a6d60bda8
rajat-k-27 <chakrabortyrajat3689@gmail.com>
2025-05-21 14:52:50 +0530

    ok3

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-import {cartographer} from "@replit/vite-plugin-cartographer";
 
 export default defineConfig({
   plugins: [
     react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-        cartographer()
-        ]
-      : []),
   ],
   resolve: {
     alias: {
